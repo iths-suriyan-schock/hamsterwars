d556d88a02bfc12e3d9a28ebe2362ddecbb122e8
iths-suriyan-schock <54362647+iths-suriyan-schock@users.noreply.github.com>
2020-07-29 12:40:12 +0200

    Update hamsters.js

--- routes/hamsters.js
+++ routes/hamsters.js
@@ -6,8 +6,6 @@ const router = new Router();
 router.get('/', async (req, res) => {
     
     try {
-        // Create funtions to create snapshot of DB collections of hamsters (use snapShot.forEach() and then hamsters.push()
-        // Create an empty array for hamsters first, a placeholder for us to use to push in the new data
         db.collection('hamsters').get().then(snapShot => {
             
             let hamsters = []
@@ -44,7 +42,7 @@ router.get('/random', async (req, res) => {
 router.get('/:id', async (req, res) => {
     
     try {
-        // Create funtions to get and show ID of hamsters
+        // Create funtions to GET and show ID of hamsters
         let hamsters = []
         let snapShot = await db.collection('hamsters').where("id", "==", parseInt(req.params.id)).get()
         snapShot.forEach(doc => {
@@ -105,67 +103,3 @@ router.put('/:id/result', async (req, res) => {
 });
 
 module.exports = router;
-// /hamsters/:id/result Updates wins/defeats and games with +1 on requested hamsterobject (PUT)
-// router.put('/:id([0-9]+)/result', async (req, res) => {
-
-//     try {
-        
-//         // Get all hamsters with the matching id from firebase
-//         let snapshot = await db
-//         .collection('hamsters')
-//         .where('id', '==', parseInt(req.params.id))
-//         .get()
-
-//         // Check if an object was found
-//         if (snapshot.size === 0) {
-                
-//             res.status(400).send({ msg: 'Id does not exist'});
-        
-//         } else {
-
-//             // Loop through results
-//             snapshot.forEach(doc => {
-                
-//                 // Check that only values 0 or 1 are passed in the request body
-//                 if (req.body.wins !== (1 || 0) && req.body.defeats !== (1 || 0)) {
-                    
-//                     res.status(400).send({msg: 'Values can only be 1 or 0.'})
-                    
-//                 } else {
-                    
-//                     // Check that the passed values are not equal
-//                     if(req.body.wins !== req.body.defeats) {
-                        
-//                         let hamster = doc.data(); // Store firebase-object as json in variable
-//                         let wins = req.body.wins;
-//                         let defeats = req.body.defeats;
-                        
-//                         // If no value is set in body then value should be 0 (to prevent changing the db-value to null)
-//                         hamster.wins += wins ? wins : 0;
-//                         hamster.defeats += defeats ? defeats : 0;
-//                         hamster.games += 1;
-                        
-//                         // Update database with new values
-//                         db
-//                         .collection('hamsters')
-//                         .doc(doc.id)
-//                         .update(hamster)
-//                         .catch(err => { throw err })
-                        
-//                         res.status(201).send({msg: `Updated hamster ${req.params.id}`})
-                        
-//                     } else {
-                        
-//                         res.status(400).send({msg: 'Values cannot be equal'})
-                        
-//                     }
-//                 }
-//             })
-//         }
-//     }
-//     catch (err) {
-
-//         res.status(500).send(err);
-        
-//     }
-// })
